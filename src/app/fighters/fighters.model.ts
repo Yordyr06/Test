f9e977ed289dee821780a8f87ca6b3d126105f7b
Yordy Almonte <yoalmonte.dev@outlook.com>
2026-07-02 19:08:25 -0400

    feat(fighters): Add model and seed data
    - Se define el DTO del modelo.
    - Se agrega archivo seed.

---
+++ src/app/fighters/fighters.model.ts
@@ -0,0 +1,11 @@
+export interface Fighter {
+  id: string;
+  name: string;
+  nickname: string;
+  weightDivision: string;
+  country: string;
+  wins: number;
+  losses: number;
+  draws: number;
+  active: boolean;
+}
